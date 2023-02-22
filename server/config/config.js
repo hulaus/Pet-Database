89a43c5387bb85a9d69eb98dba3204737b50d9c9
Rodrigo Regalado <r.regalado6204@gmail.com>
2023-02-22 11:34:18 -0800

    changed config lines to strings

--- server/config/config.js
+++ server/config/config.js
@@ -2,25 +2,25 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: postgres,
-    password: ThisPasswordIsWeak,
-    database: postgres,
-    host: db.mduemnearpuqwcvpgnph.supabase.co,
+    username: 'postgres',
+    password: 'ThisPasswordIsWeak',
+    database: 'postgres',
+    host: 'db.mduemnearpuqwcvpgnph.supabase.co',
     logging: true,
     dialect: "postgres"
   },
   test: {
-    username: postgres,
-    password: ThisPasswordIsWeak,
-    database: postgres,
-    host: db.mduemnearpuqwcvpgnph.supabase.co,
+    username: 'postgres',
+    password: 'ThisPasswordIsWeak',
+    database: 'postgres',
+    host: 'db.mduemnearpuqwcvpgnph.supabase.co',
     dialect: "postgres"
   },
   production: {
-    username: postgres,
-    password: ThisPasswordIsWeak,
-    database: postgres,
-    host: db.mduemnearpuqwcvpgnph.supabase.co,
+    username: 'postgres',
+    password: 'ThisPasswordIsWeak',
+    database: 'postgres',
+    host: 'db.mduemnearpuqwcvpgnph.supabase.co',
     dialect: "postgres"
   }
 }
