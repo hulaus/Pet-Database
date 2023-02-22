c567c6ef28f9418618a43a645985016828ca8fd3
Jo Carr <hulaus21@gmail.com>
2023-02-22 10:49:45 -0800

    hard Coded DB Username, Password, Name & Host

--- server/config/config.js
+++ server/config/config.js
@@ -2,25 +2,25 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: postgres,
+    password: ThisPasswordIsWeak,
+    database: postgres,
+    host: db.mduemnearpuqwcvpgnph.supabase.co,
     logging: true,
     dialect: "postgres"
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: postgres,
+    password: ThisPasswordIsWeak,
+    database: postgres,
+    host: db.mduemnearpuqwcvpgnph.supabase.co,
     dialect: "postgres"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: postgres,
+    password: ThisPasswordIsWeak,
+    database: postgres,
+    host: db.mduemnearpuqwcvpgnph.supabase.co,
     dialect: "postgres"
   }
 }
